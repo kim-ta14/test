385403d3db605f1bd0e5956ee6d2524607858060
kim-ta14 <takuyacronald7@gmail.com>
2019-03-05 13:41:39 +0900

    3/5

--- template/sql/ecsite.sql
+++ template/sql/ecsite.sql
@@ -1,9 +1,9 @@
 set names utf8;
 set foreign_key_checks = 0;
-drop database if exists ecsite;
+drop database if exists ecsite_kimura;
 
-create database if not exists ecsite;
-use ecsite;
+create database if not exists ecsite_kimura;
+use ecsite_kimura;
 
 drop table if exists login_user_transaction;
 
