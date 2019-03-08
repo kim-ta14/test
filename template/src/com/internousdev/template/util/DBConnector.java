4f41637bddd2a1a1ff3c630d2616c99d5c4ea446
木村　拓也 <kimuratakuya@kimuratakuyanoMacBook-Air.local>
2019-03-08 10:20:30 +0900

    3/8

--- template/src/com/internousdev/template/util/DBConnector.java
+++ template/src/com/internousdev/template/util/DBConnector.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DBConnector {
 
 	private static String driverName ="com.mysql.jdbc.Driver";
-	private static String url = "jdbc:mysql://localhost/ecsite_kimura";
+	private static String url = "jdbc:mysql://localhost:8889/ecsite_kimura";
 	private static String user = "root";
-	private static String password = "mysql";
+	private static String password = "root";
 
 public Connection getConnection() {
 	Connection con = null;
